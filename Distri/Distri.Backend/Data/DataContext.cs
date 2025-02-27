bff18e93413995cd9c964f9dccd905557d25d15b
Gabriel Barjau <gbarjau@gmail.com>
2025-02-27 15:03:49 -0300

    Inicio, configuracion de conexion y primer entidad paises

---
+++ Distri/Distri.Backend/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Distri.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Distri.Backend.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+
+    }
+}
