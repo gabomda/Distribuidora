780e6573713f6735258950171f14f5556b6b4998
Gabriel Barjau <gbarjau@gmail.com>
2025-03-06 19:13:02 -0300

    CRUD Categorias finalizado

---
+++ Distri/Distri.Frontend/Pages/Categories/CategoryCreate.razor.cs
@@ -0,0 +1,46 @@
+﻿using CurrieTechnologies.Razor.SweetAlert2;
+using Distri.Frontend.Pages.Countries;
+using Distri.Frontend.Repositories;
+using Distri.Shared.Entities;
+using Microsoft.AspNetCore.Components;
+
+namespace Distri.Frontend.Pages.Categories
+{
+    public partial class CategoryCreate
+    {
+
+        private Category category = new();
+        private CategoryForm? categoryForm;
+        [Inject] private IRepository Repository { get; set; } = null!;
+        [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+        [Inject] private SweetAlertService SweetAlertService { get; set; } = null!;
+
+        private async Task CreateAsync()
+        {
+            var responseHttp = await Repository.PostAsync("/api/categories", category);
+            if (responseHttp.Error)
+            {
+                var message = await responseHttp.GetErrorMessageAsync();
+                await SweetAlertService.FireAsync("Error", message);
+                return;
+            }
+            Return();
+
+            var toast = SweetAlertService.Mixin(new SweetAlertOptions
+            {
+                Toast = true,
+                Position = SweetAlertPosition.BottomEnd,
+                ShowConfirmButton = true,
+                Timer = 3000
+            });
+            await toast.FireAsync(icon: SweetAlertIcon.Success, message: "Registro creado con éxito.");
+        }
+
+        private void Return()
+        {
+            categoryForm!.FormPostedSuccessfully = true;
+            NavigationManager.NavigateTo("/categories");
+        }
+
+    }
+}
